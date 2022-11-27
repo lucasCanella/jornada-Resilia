8dbb705b583ad4429ca9e62be41a85f7d8e903c6
Lucas Canella <lucas.fariascanella@gmail.com>
2022-11-27 19:17:31 -0300

    Update musicas-queries.sql

--- Módulo3-BancoDeDados_CRUD_e_Queries_Com_SQL/músicas/musicas-queries/musicas-queries.sql
+++ Módulo3-BancoDeDados_CRUD_e_Queries_Com_SQL/músicas/musicas-queries/musicas-queries.sql
@@ -37,7 +37,7 @@ inner join invoiceline on invoice.invoiceid = invoiceline.invoiceid
 inner join track on invoiceline.trackid = track.trackid
 inner join album on track.albumid = album.albumid
 inner join artist on album.artistid = artist.artistid
-group by customer.country having country = 'Brazil'
+group by customer.country having country = 'Brazil';
 
 -- 5) Selecione os nomes dos artistas mais tocados agrupados por países de origem dos usuários;
 
@@ -47,13 +47,13 @@ inner join invoiceline on invoice.invoiceid = invoiceline.invoiceid
 inner join track on invoiceline.trackid = track.trackid
 inner join album on track.albumid = album.albumid
 inner join artist on album.artistid = artist.artistid
-group by customer.country
+group by customer.country;
 
 -- 6) Selecione o nome dos artistas associados com o título dos seus álbuns;
 
 select artist.artistid, artist.name, album.title from artist 
 inner join album on artist.artistid = album.artistid
-order by artist.artistid
+order by artist.artistid;
 
 -- 7) Selecione o nome dos artistas associados com o nome das músicas (Track.name) de forma paginada com 10 resultados por página;
 
@@ -70,22 +70,40 @@ group by playlist.name order by count(playlisttrack.trackid) desc;
 
 -- 9) Exiba o nome do gênero e a média de tempo das músicas em milisegundos agrupadas por gênero;
 
-
+select genre.name, avg(track.milliseconds) from genre
+inner join track on genre.genreid = track.genreid
+group by genre.name order by avg(track.milliseconds);
 
 -- 10) Exiba o nome do álbum que possui a maior média de tempo em milisegundos cujo o gênero musical é Rock;
 
-
+select album.title, avg(rock_songs.milliseconds) from (select * from track where genreid = 1) rock_songs
+inner join album on album.albumid = rock_songs.albumid
+group by album.title order by avg(rock_songs.milliseconds) desc limit 1;
 
 -- 11) Selecione o nome dos consumidores que tiveram as contas (invoice.total) maiores que a média;
 
+select customer.firstname,customer.lastname, invoice.total from customer 
+inner join invoice on customer.customerid =invoice.customerid
+where invoice.total > (select avg(invoice.total) from invoice)
+order by invoice.total;
 
--- 12) Selecione os álbuns do artiste que possui a maior quantidade de álbuns lançados;
+-- 12) Selecione os álbuns do artista que possui a maior quantidade de álbuns lançados;
 
+select artistid, count(title) from album
+group by artistid 
+order by count(title) desc limit 1;
 
-
+select artist.artistid, artist.name,album.albumid, album.title from album
+inner join artist on album.artistid = artist.artistid
+where album.artistid = (select artistid from album
+                        group by artistid 
+                        order by count(title) desc limit 1);
+                        
 -- 13) Selecione o nome da playlist que possui a maior quantidade de músicas (Track.name);
 
-
+select Playlist.name, count(trackid) num_musicas from Playlist
+inner join playlisttrack on Playlist.Playlistid = playlisttrack.Playlistid
+group by Playlist.name order by count(trackid) desc limit 1
 
 -- 14) Selecione o nome do gênero musical que possui a menor quantidade de músicas (Track.name);
 
